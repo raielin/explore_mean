a86ed236d8508f096c7bdbfde349152064d28325
raielin@gmail.com <raielin@gmail.com>
2015-03-23 12:23:18 -0400

    Create posts variable on  to display list of posts.

--- login_app/app.js
+++ login_app/app.js
@@ -4,5 +4,12 @@ app.controller('MainCtrl', [
   '$scope',
   function($scope){
     $scope.test = 'Hello world!';
+    $scope.posts = [
+      'post 1',
+      'post 2',
+      'post 3',
+      'post 4',
+      'post 5'
+    ];
   }
 ])
