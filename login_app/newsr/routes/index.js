7a6bdc1b1ec2677aa95365c64632236a86353d28
raielin@gmail.com <raielin@gmail.com>
2015-03-24 17:28:44 -0400

    Create routes to grap post object by ID and GET post.

--- login_app/newsr/routes/index.js
+++ login_app/newsr/routes/index.js
@@ -37,4 +37,31 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+/* Grab Post object by ID */
+// Use Express's param() function to automatically preload post object.
+// Use Mongoose's query interface which provides a more flexible way of interacting with the database.
+// Any route URL with :post in it will run this function first to retrieve the post object from the datase, then attach it to the `req` object of the route handler function.
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function(err, post) {
+    if (err) {
+      return next(err);
+    }
+    if (!post) {
+      return next(new Error('can\'t find post'));
+    }
+    req.post = post;
+    return next();
+  });
+});
+
+/* GET post */
+// Will grab post ID as written in router.param and attach it to the `req` argument passed into route handler function.
+router.get('/posts/:post', function(req, res) {
+  // `req` already has the post ID attached to it when this runs.
+  res.json(req.post);
+});
+
 module.exports = router;
+
