9ee537b4382e8eb957e74de4cd2af9e7214fce3b
raielin@gmail.com <raielin@gmail.com>
2015-03-24 14:27:08 -0400

    Add comment about ObjectId datatype. Fix formatting for readibility.

--- login_app/newsr/models/Posts.js
+++ login_app/newsr/models/Posts.js
@@ -3,7 +3,11 @@ var mongoose = require('mongoose');
 var PostSchema = new mongoose.Schema({
   title: String,
   link: String,
-  upvotes: {type: Number, default: 0},
+  upvotes: {
+    type: Number,
+    default: 0
+  },
+  // ObjectId datatype refers to a MongodB ObjectId stored in the database. Mongoose will retreive the object 'Comment' object referenced by the ID.
   comments: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Comment'
