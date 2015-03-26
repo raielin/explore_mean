f412b5dca048b66f9bf3974a86fa2c9ee058dcd4
raielin@gmail.com <raielin@gmail.com>
2015-03-25 22:23:44 -0400

    Add passport to our application.

--- login_app/newsr/app.js
+++ login_app/newsr/app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 var mongoose =  require('mongoose');
                 require('./models/Posts');
                 require('./models/Comments');
+                require('./models/Users');
+                require('./config/passport');
+var passport = require('passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -27,6 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
