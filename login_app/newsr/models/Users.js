3479701a1ff2dcaaf8dffaab8d1d00ca2a1cbc4d
raielin@gmail.com <raielin@gmail.com>
2015-03-25 21:58:07 -0400

    Create setPassword() method on User model that takes a password then generates a salt and associated password hash. Use pbkdf2() with crypto for hashing.

--- login_app/newsr/models/Users.js
+++ login_app/newsr/models/Users.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+// using pbkdf2() function to hash our passwords which ships with node's native crypto module
+var crypto = require('crypto');
 
 var UserSchema = new mongoose.Schema({
   username: {
@@ -11,5 +13,10 @@ var UserSchema = new mongoose.Schema({
   // generate and save salt whenever password is set
   salt: String
 });
+
+UserSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
 
 mongoose.model('User', UserSchema);
