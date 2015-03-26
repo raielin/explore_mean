465bd7ee3fb4eb70311265db5c99857d2b94e5a4
raielin@gmail.com <raielin@gmail.com>
2015-03-25 22:11:39 -0400

    Require jsonwebtoken. Create generateJWT() instance method to generate token.

--- login_app/newsr/models/Users.js
+++ login_app/newsr/models/Users.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 // using pbkdf2() function to hash our passwords which ships with node's native crypto module
 var crypto = require('crypto');
+// using JWT to generate tokens for session management
+var jwt = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema({
   username: {
@@ -26,5 +28,25 @@ UserSchema.methods.validPassword = function(password) {
 
   return this.hash === hash;
 };
+
+// generateJWT() instance method for generating a JWT token for the user with jsonwebtoken() module
+UserSchema.methods.generateJWT = function() {
+  // set expiration to 60 days
+  var today = new Date();
+  var exp = new Date(today);
+
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  },
+  'SECRET');
+};
+
+/* Note on jwt.sign() */
+// first argument is payload that gets signed. both server and client have access to payload.
+// second argument is the secret used to sign our tokens. it's hard-coded here in this example, but normally, ALWAYS use an environment variable for referencing the secret and keep it out of codebase.
 
 mongoose.model('User', UserSchema);
